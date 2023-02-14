0660d4e12ba7875ae48a0de0421eabe71184f128
Rizqy-617 <112744959+Rizqy-617@users.noreply.github.com>
2023-02-14 08:00:08 +0700

    Update api.js

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create base URL API
 export const API = axios.create({
-  baseURL: "https://sassy-use-production.up.railway.app/api/v1/",
+  baseURL: "jadi-production.up.railway.app/api/v1/",
 });
 // Set Authorization Token Header
 export const setAuthToken = (token) => {
